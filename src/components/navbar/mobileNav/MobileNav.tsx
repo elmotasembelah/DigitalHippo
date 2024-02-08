0d1bb26bb087c0e707a54080cbb5f7240e2e112f
elmotasembelah <79071279+elmotasembelah@users.noreply.github.com>
2024-02-08 14:19:03 +0200

    feat&refacotr: added feature to close mobilenav on click outside and refactored its code

--- src/components/navbar/mobileNav/MobileNav.tsx
+++ src/components/navbar/mobileNav/MobileNav.tsx
@@ -1,42 +1,36 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 import { Menu, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
 
 const MobileNav = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const mobileNavRef = useRef<HTMLDivElement | null>(null);
 
-  const pathname = usePathname();
-
-  // whenever we click an item in the menu and navigate away, we want to close the menu
-  useEffect(() => {
+  // to close the menu when we click outside the menu
+  useOnClickOutside(mobileNavRef, () => {
     setIsOpen(false);
-  }, [pathname]);
+  });
 
-  // when we click the path we are currently on, we still want the mobile menu to close,
-  // however we cant rely on the pathname for it because that won't change (we're already there)
-  const closeOnCurrent = (href: string) => {
-    if (pathname === href) {
-      setIsOpen(false);
-    }
+  const openMenu = () => {
+    setIsOpen(true);
+    document.body.classList.add("overflow-hidden");
   };
-
-  // remove second scrollbar when mobile menu is open
-  useEffect(() => {
-    if (isOpen) document.body.classList.add("overflow-hidden");
-    else document.body.classList.remove("overflow-hidden");
-  }, [isOpen]);
+  const closeMenu = () => {
+    setIsOpen(false);
+    document.body.classList.remove("overflow-hidden");
+  };
 
   if (!isOpen)
     return (
       <button
         type="button"
-        onClick={() => setIsOpen(true)}
-        className="lg:hidden relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
+        onClick={openMenu}
+        className="lg:hidden relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400 "
       >
         <Menu className="h-6 w-6" aria-hidden="true" />
       </button>
@@ -48,13 +42,16 @@ const MobileNav = () => {
         <div className="fixed inset-0 bg-black bg-opacity-25" />
       </div>
 
-      <div className="fixed overflow-y-scroll overscroll-y-none inset-0 z-40 flex lg:hidden">
+      <div className="fixed overflow-y-scroll overscroll-y-none inset-0 z-40 flex lg:hidden  ">
         <div className="w-4/5">
-          <div className="relative flex w-full max-w-sm flex-col overflow-y-auto bg-white pb-12 shadow-xl">
-            <div className="flex px-4 pb-2 pt-5">
+          <div
+            className="relative flex w-full max-w-sm flex-col overflow-y-auto bg-white pb-12 shadow-xl"
+            ref={mobileNavRef}
+          >
+            <div className="flex justify-between px-4 pb-2 pt-5">
               <button
                 type="button"
-                onClick={() => setIsOpen(false)}
+                onClick={closeMenu}
                 className="relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
               >
                 <X className="h-6 w-6" aria-hidden="true" />
@@ -79,18 +76,19 @@ const MobileNav = () => {
                     <div className="grid grid-cols-2 gap-y-10 gap-x-4">
                       {category.featured.map((item) => (
                         <div key={item.name} className="group relative text-sm">
-                          <div className="relative aspect-square overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                            <Image
-                              fill
-                              src={item.imageSrc}
-                              alt="product category image"
-                              className="object-cover object-center"
-                            />
-                          </div>
                           <Link
+                            onClick={closeMenu}
                             href={item.href}
-                            className="mt-6 block font-medium text-gray-900"
+                            className=" block font-medium text-gray-900 text-center"
                           >
+                            <div className="mb-4 relative aspect-square overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75 ">
+                              <Image
+                                fill
+                                src={item.imageSrc}
+                                alt="product category image"
+                                className="object-cover object-center"
+                              />
+                            </div>
                             {item.name}
                           </Link>
                         </div>
@@ -104,7 +102,7 @@ const MobileNav = () => {
             <div className="space-y-6 border-t border-gray-200 px-4 py-6">
               <div className="flow-root">
                 <Link
-                  onClick={() => closeOnCurrent("/sign-in")}
+                  onClick={closeMenu}
                   href="/sign-in"
                   className="-m-2 block p-2 font-medium text-gray-900"
                 >
@@ -113,7 +111,7 @@ const MobileNav = () => {
               </div>
               <div className="flow-root">
                 <Link
-                  onClick={() => closeOnCurrent("/sign-up")}
+                  onClick={closeMenu}
                   href="/sign-up"
                   className="-m-2 block p-2 font-medium text-gray-900"
                 >
