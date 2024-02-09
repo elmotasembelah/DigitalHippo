1b48709ad88ce71d298bb12414393a586326fd84
elmotasembelah <79071279+elmotasembelah@users.noreply.github.com>
2024-02-09 13:57:48 +0200

    style(ProductReel):add padding x on products grid

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -64,7 +64,7 @@ const ProductReel = (props: ProductReelProps) => {
 
       <div className="relative">
         <div className="mt-6 flex items-center w-full">
-          <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:grid-cols-4 md:gap-x-6 md:gap-y-10 lg:gap-x-8 ">
+          <div className="w-full px-4 grid grid-cols-2 gap-x-4 gap-y-10 sm:grid-cols-4 md:gap-x-6 md:gap-y-10 lg:gap-x-8 lg:px-0 ">
             {map.map((product, i) => (
               <ProductListing
                 key={`product-${i}`}
