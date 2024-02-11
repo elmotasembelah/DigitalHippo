58ac09516cd1bf2726a4b32afeeb8a3a84632dc6
elmotasembelah <79071279+elmotasembelah@users.noreply.github.com>
2024-02-11 17:21:43 +0200

    fix(paymentRouter):fix a type in pollRequestStatus

--- dist/trpc/payment-router.js
+++ dist/trpc/payment-router.js
@@ -43,12 +43,12 @@ var server_1 = require("@trpc/server");
 var get_payload_1 = require("../get-payload");
 var stripe_1 = require("../lib/stripe");
 exports.paymentRouter = (0, trpc_1.router)({
-    createSession: trpc_1.privateProcedure
+    createSesstion: trpc_1.privateProcedure
         .input(zod_1.z.object({ productIds: zod_1.z.array(zod_1.z.string()) }))
         .mutation(function (_a) {
         var ctx = _a.ctx, input = _a.input;
         return __awaiter(void 0, void 0, void 0, function () {
-            var user, productIds, payload, products, filteredProducts, order, line_items, stripeSession, err_1;
+            var user, productIds, payload, products, filteredProducts, order, line_items, stripeSession, error_1;
             return __generator(this, function (_b) {
                 switch (_b.label) {
                     case 0:
@@ -70,12 +70,14 @@ exports.paymentRouter = (0, trpc_1.router)({
                             })];
                     case 2:
                         products = (_b.sent()).docs;
-                        filteredProducts = products.filter(function (prod) { return Boolean(prod.priceId); });
+                        filteredProducts = products.filter(function (product) {
+                            return Boolean(product.priceId);
+                        });
                         return [4 /*yield*/, payload.create({
                                 collection: "orders",
                                 data: {
-                                    _isPaid: false,
-                                    products: filteredProducts.map(function (prod) { return prod.id; }),
+                                    _isPaid: false, // this will be true when we are sure that the order has been payed
+                                    products: filteredProducts.map(function (product) { return product.id; }),
                                     user: user.id,
                                 },
                             })];
@@ -89,9 +91,10 @@ exports.paymentRouter = (0, trpc_1.router)({
                             });
                         });
                         line_items.push({
-                            price: "price_1OCeBwA19umTXGu8s4p2G3aX",
+                            price: "price_1OfPMWFTPqNRH4gUVNzAbbTn", // this is the price id of the transaction fe from stripe dashboard (9.38.14 video)
                             quantity: 1,
                             adjustable_quantity: {
+                                // can i put more than one of this item, in our case no
                                 enabled: false,
                             },
                         });
@@ -101,19 +104,21 @@ exports.paymentRouter = (0, trpc_1.router)({
                         return [4 /*yield*/, stripe_1.stripe.checkout.sessions.create({
                                 success_url: "".concat(process.env.NEXT_PUBLIC_SERVER_URL, "/thank-you?orderId=").concat(order.id),
                                 cancel_url: "".concat(process.env.NEXT_PUBLIC_SERVER_URL, "/cart"),
-                                payment_method_types: ["card", "paypal"],
-                                mode: "payment",
+                                payment_method_types: ["card"],
+                                mode: "payment", // only one payment in a subscription
                                 metadata: {
-                                    userId: user.id,
+                                    // this is given back to us when the session is successful
+                                    userid: user.id,
                                     orderId: order.id,
                                 },
                                 line_items: line_items,
                             })];
                     case 5:
                         stripeSession = _b.sent();
-                        return [2 /*return*/, { url: stripeSession.url }];
+                        return [2 /*return*/, { url: stripeSession.url }]; // success url
                     case 6:
-                        err_1 = _b.sent();
+                        error_1 = _b.sent();
+                        console.log(error_1);
                         return [2 /*return*/, { url: null }];
                     case 7: return [2 /*return*/];
                 }
@@ -135,6 +140,7 @@ exports.paymentRouter = (0, trpc_1.router)({
                         payload = _b.sent();
                         return [4 /*yield*/, payload.find({
                                 collection: "orders",
+                                limit: 1,
                                 where: {
                                     id: {
                                         equals: orderId,
--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -1,15 +1,16 @@
-import { z } from "zod";
-import { privateProcedure, router } from "./trpc";
+import { boolean, z } from "zod";
+import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { getPayloadClient } from "../get-payload";
+import { Products } from "@/collections/products/Products";
 import { stripe } from "../lib/stripe";
 import type Stripe from "stripe";
 
 export const paymentRouter = router({
-  createSession: privateProcedure
+  createSesstion: privateProcedure
     .input(z.object({ productIds: z.array(z.string()) }))
     .mutation(async ({ ctx, input }) => {
-      const { user } = ctx;
+      const { user } = ctx; // since we passed the user in the middle ware, we get to use it here in a typesafe manner plus konwing it can't be false
       let { productIds } = input;
 
       if (productIds.length === 0) {
@@ -27,17 +28,20 @@ export const paymentRouter = router({
         },
       });
 
-      const filteredProducts = products.filter((prod) => Boolean(prod.priceId));
+      const filteredProducts = products.filter((product) =>
+        Boolean(product.priceId)
+      );
 
       const order = await payload.create({
         collection: "orders",
         data: {
-          _isPaid: false,
-          products: filteredProducts.map((prod) => prod.id),
+          _isPaid: false, // this will be true when we are sure that the order has been payed
+          products: filteredProducts.map((product) => product.id),
           user: user.id,
         },
       });
 
+      //   we use this to tell stripe what are the items the user is paying for
       const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
 
       filteredProducts.forEach((product) => {
@@ -48,9 +52,10 @@ export const paymentRouter = router({
       });
 
       line_items.push({
-        price: "price_1OCeBwA19umTXGu8s4p2G3aX",
+        price: "price_1OfPMWFTPqNRH4gUVNzAbbTn", // this is the price id of the transaction fe from stripe dashboard (9.38.14 video)
         quantity: 1,
         adjustable_quantity: {
+          // can i put more than one of this item, in our case no
           enabled: false,
         },
       });
@@ -59,17 +64,20 @@ export const paymentRouter = router({
         const stripeSession = await stripe.checkout.sessions.create({
           success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
           cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
-          payment_method_types: ["card", "paypal"],
-          mode: "payment",
+          payment_method_types: ["card"],
+          mode: "payment", // only one payment in a subscription
           metadata: {
-            userId: user.id,
+            // this is given back to us when the session is successful
+            userid: user.id,
             orderId: order.id,
           },
           line_items,
         });
 
-        return { url: stripeSession.url };
-      } catch (err) {
+        return { url: stripeSession.url }; // success url
+      } catch (error) {
+        console.log(error);
+
         return { url: null };
       }
     }),
@@ -79,9 +87,9 @@ export const paymentRouter = router({
       const { orderId } = input;
 
       const payload = await getPayloadClient();
-
       const { docs: orders } = await payload.find({
         collection: "orders",
+        limit: 1,
         where: {
           id: {
             equals: orderId,
