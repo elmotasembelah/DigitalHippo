db5d15140eaadd380093cdd4e2fa5c03894aa548
elmotasembelah <79071279+elmotasembelah@users.noreply.github.com>
2024-02-12 17:00:02 +0200

    style(mobilenav):put categories in accordions

--- src/components/navbar/mobileNav/MobileNav.tsx
+++ src/components/navbar/mobileNav/MobileNav.tsx
@@ -3,10 +3,17 @@
 import { PRODUCT_CATEGORIES } from "@/config";
 import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 import { Menu, X } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
 import { useRef, useState } from "react";
 import UserOptions from "./userOptions/UserOptions";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { createMigration } from "payload/database";
+import { AccordionContent } from "@radix-ui/react-accordion";
+import Link from "next/link";
+import Image from "next/image";
 
 const MobileNav = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -43,10 +50,10 @@ const MobileNav = () => {
         <div className="fixed inset-0 bg-black bg-opacity-25" />
       </div>
 
-      <div className="fixed overflow-y-scroll overscroll-y-none inset-0 z-40 flex lg:hidden  ">
+      <div className="fixed overflow-y-scroll overscroll-y-none inset-0 z-40 flex lg:hidden   ">
         <div className="w-4/5">
           <div
-            className="relative flex w-full max-w-sm flex-col overflow-y-auto bg-white pb-12 shadow-xl"
+            className="relative flex w-full max-w-sm flex-col overflow-y-auto bg-white pb-12 shadow-xl h-full"
             ref={mobileNavRef}
           >
             <div className="flex justify-between px-4 pb-2 pt-5">
@@ -59,49 +66,48 @@ const MobileNav = () => {
               </button>
             </div>
 
-            <div>
-              <UserOptions closeMenu={closeMenu} />
-            </div>
-
-            <div className="mt-2">
+            <div className="mt-4">
               <ul>
                 {PRODUCT_CATEGORIES.map((category) => (
-                  <li
-                    key={category.label}
-                    className="space-y-10 px-4 pb-8 pt-10 border-b border-gray-200"
-                  >
-                    <div className="">
-                      <div className="-mb-px flex">
-                        <p className="border-transparent text-gray-900 flex-1 whitespace-nowrap border-b-2 py-4 text-base font-medium text-center">
-                          {category.label}
-                        </p>
-                      </div>
-                    </div>
-
-                    <div className="grid grid-cols-2 gap-y-10 gap-x-4">
-                      {category.featured.map((item) => (
-                        <div key={item.name} className="group relative text-sm">
-                          <Link
-                            onClick={closeMenu}
-                            href={item.href}
-                            className=" block font-medium text-gray-900 text-center"
-                          >
-                            <div className="mb-4 relative aspect-square overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75 ">
-                              <Image
-                                fill
-                                src={item.imageSrc}
-                                alt="product category image"
-                                className="object-cover object-center"
-                              />
-                            </div>
-                            {item.name}
-                          </Link>
-                        </div>
-                      ))}
-                    </div>
+                  <li key={category.label} className="space-y-10 px-4 pb-0  ">
+                    <Accordion type="single" collapsible>
+                      <AccordionItem value={category.value}>
+                        <AccordionTrigger>{category.label}</AccordionTrigger>
+                        <AccordionContent className="">
+                          <div className="grid grid-cols-2 gap-y-10 gap-x-4">
+                            {category.featured.map((item) => (
+                              <div
+                                key={item.name}
+                                className="group relative text-sm"
+                              >
+                                <Link
+                                  onClick={closeMenu}
+                                  href={item.href}
+                                  className=" block font-medium text-gray-900 text-center"
+                                >
+                                  <div className="mb-4 relative aspect-square overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75 ">
+                                    <Image
+                                      fill
+                                      src={item.imageSrc}
+                                      alt="product category image"
+                                      className="object-cover object-center"
+                                    />
+                                  </div>
+                                  {item.name}
+                                </Link>
+                              </div>
+                            ))}
+                          </div>
+                        </AccordionContent>
+                      </AccordionItem>
+                    </Accordion>
                   </li>
                 ))}
               </ul>
+            </div>
+
+            <div>
+              <UserOptions closeMenu={closeMenu} />
             </div>
           </div>
         </div>
